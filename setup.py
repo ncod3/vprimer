31cd70b3472bd94b8d623d89ac7ee87cd29e210f
ncod3 <ncod3@sunny-side-st.com>
2023-04-03 15:39:16 +0900

    2023.0403.1539

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vprimer",
-    version="1.0.0",
+    version="1.0.1",
     author="satoshi-natsume",
     author_email="s-natsume@ibrc.or.jp",
     description="V-primer",
